AKoskovich/android_kernel_nothing_sm7635

misc: haptic: Add a space after ics_haptic logs
Change-Id: I31ea0204cb6efff40d4002e0aed7fb09df61eb96

drivers/misc/haptic/haptic_drv.h
@@ -14,13 +14,13 @@
 #define AAC_RICHTAP_SUPPORT
 
 #define ics_err(format, ...) \
-	pr_err("[ics_haptic]" format, ##__VA_ARGS__)
+	pr_err("[ics_haptic] " format, ##__VA_ARGS__)
 
 #define ics_info(format, ...) \
-	pr_err("[ics_haptic]" format, ##__VA_ARGS__)
+	pr_err("[ics_haptic] " format, ##__VA_ARGS__)
 
 #define ics_dbg(format, ...) \
-	pr_err("[ics_haptic]" format, ##__VA_ARGS__)
+	pr_err("[ics_haptic] " format, ##__VA_ARGS__)
 
 #define check_error_return(ret)	\
 	if (ret < 0) {	\

Mashopy/android_kernel_nothing_mt6886

drivers/misc/haptic/haptic_drv.h
@@ -12,13 +12,13 @@
 #define AAC_RICHTAP_SUPPORT
 
 #define ics_err(format, ...) \
-	pr_info("[ics_haptic]" format, ##__VA_ARGS__)
+	pr_info("[ics_haptic] " format, ##__VA_ARGS__)
 
 #define ics_info(format, ...) \
-	pr_info("[ics_haptic]" format, ##__VA_ARGS__)
+	pr_info("[ics_haptic] " format, ##__VA_ARGS__)
 
 #define ics_dbg(format, ...) \
-	pr_info("[ics_haptic]" format, ##__VA_ARGS__)
+	pr_info("[ics_haptic] " format, ##__VA_ARGS__)
 
 #define check_error_return(ret)	\
 	if (ret < 0) {	\